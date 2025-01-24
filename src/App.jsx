d5c169940e76019bfa62f7cce4c47b088d527a9c
nadiaramirezx <ramirezznadia@outlook.com>
2025-01-24 15:16:12 -0500

    REPO SUBIDA 24-ENE

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import ApiNasa from './components/NasaApi'
+
+
+function App() {
+
+  return (
+    <>
+      <ApiNasa/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NasaApi.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import "../styles/Api.css"
+
+// Definimos el tipo de dato que esperamos obtener
+interface NasaData {
+   identifier: string;
+   caption: string;
+   image: string;
+   date: string;
+}
+
+
+function NasaAPi() {
+  const apiKey = "UZHrDmWJ1Yoqq8ihKJo5FxHk5ImTkmqniwKTpQQj"; // Reemplaza 'TU_API_KEY' con tu clave real
+  const url = `https://api.nasa.gov/EPIC/api/natural/images?api_key=${apiKey}`;
+
+  // Definición de estados del componente
+  const [images, setImages] = useState<NasaData[]>([]); // Aquí se jalan los datos de la API
+  const [selectedImage, setSelectedImage] = useState<NasaData | null>(null);
+
+  // Función que llama a la API usando fetch
+  const fetchData = () => {
+    return fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('La conexion fallo'); //mensaje de error
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data); // Verifica los datos a recibir
+        setImages(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  //hook que carga los datos desde el localstorage
+  useEffect(() => {
+   const storedImage = localStorage.getItem("selectedImage");
+   if (storedImage) {
+     setSelectedImage(JSON.parse(storedImage));
+   }
+   fetchData();
+ }, []);
+
+ // Guardar la  tarjeta seleccionado en localStorage
+ const handleImageClick = (image: NasaData) => {
+   setSelectedImage(image);
+   localStorage.setItem("selectedImage", JSON.stringify(image));
+ };
+
+ return (
+   <div className="container">
+   {/* Mostrar la tarjeta seleccionada */}
+   {selectedImage && (
+     <div className="selected-card">
+       <h2 className="image-title">{selectedImage.caption}</h2>
+       <img
+         className="image"
+         src={`https://epic.gsfc.nasa.gov/archive/natural/${selectedImage.date
+           .split(" ")[0]
+           .replace(/-/g, "/")}/png/${selectedImage.image}.png`}
+         alt={selectedImage.caption}
+       />
+       <p className="image-date">
+         {new Date(selectedImage.date).toLocaleString()}
+       </p>
+     </div>
+   )}
+
+   {/* Lista de elementos clickeables */}
+   <ul className="image-list">
+     {images.length > 0 ? (
+       images.map((image) => (
+         <li
+           key={image.identifier}
+           className="image-list-item"
+           onClick={() => handleImageClick(image)}
+         >
+           {image.caption}
+         </li>
+       ))
+     ) : (
+       <p className="loading-text">Cargando...</p>
+     )}
+   </ul>
+ </div>
+ );
+}
+
+export default NasaAPi;
